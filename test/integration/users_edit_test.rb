939f690780cecef04e11073bd891bd964c373379
Vaughn Antonyan <vaughnanton@gmail.com>
2018-04-21 23:37:48 -0700

    Authentication test edits

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,10 +5,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-  .
-  .
-  .
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+
+    assert_template 'users/edit'
+  end
+
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name  = "Foo Bar"
