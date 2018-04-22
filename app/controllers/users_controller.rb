c31bef56bd412e860c6722531d2aa621a01bc31e
Vaughn Antonyan <vaughnanton@gmail.com>
2018-04-22 00:37:09 -0700

    Create user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+   end
 
   def show
     @user = User.find(params[:id])
@@ -8,6 +11,10 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
