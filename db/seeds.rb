674d0b479541899e4f9469ae0d0f31b147ab544b
Vaughn Antonyan <vaughnanton@gmail.com>
2018-04-23 21:24:11 -0700

    Create activation mailer structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar")
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 
 99.times do |n|
@@ -13,4 +15,6 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
-end
+               activated: true,
+               activated_at: Time.zone.now)
+             end
