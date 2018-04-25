1de45156a29ede9eeca99ee12c8847b1a99b1dda
Vaughn Antonyan <vaughnanton@gmail.com>
2018-04-24 21:55:25 -0700

    Add password reset function

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,10 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     # Converts email to all lower-case.
